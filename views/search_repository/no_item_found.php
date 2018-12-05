0c33ee7a266b70297cce75b2d7dae720dcb6a970
xhu7 <xhu7@localhost>
2018-12-05 13:18:15 -0600

    no item found in AHA

--- views/search_repository/no_item_found.php
+++ views/search_repository/no_item_found.php
@@ -1,4 +1,4 @@
 <div class="alert alert-warning">
     <strong>NO ITEMS FOUND!</strong>
-    <p>The following term was not found in bioCADDIE: <strong class="text-danger"><?php echo $searchRepoView->getSearchBuilder()->getQuery(); ?></strong></p>
+    <p>The following term was not found in AHA website: <strong class="text-danger"><?php echo $searchRepoView->getSearchBuilder()->getQuery(); ?></strong></p>
 </div>

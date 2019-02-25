8ef8700ec4032d1dad8e1874b93af7485c78eb69
yueyuexiny <xhu7@localhost>
2019-02-25 00:07:35 -0600

    improve UI

--- views/about/about.php
+++ views/about/about.php
@@ -7,18 +7,40 @@
  */
 
 ?>
-<div class="container">
-<h1 > <span style="text-align:center;background-color: #000000;color:#ffffff">OUR MISSION: </span></h1>
-    <span style="text-align:center;background-color: #000000;color:#ffffff">Develop a prototype of a cardiovascular research data discovery index.</span>
+<div class="container" style="background-color:#e56969; margin:30px auto">
+
+    <h3 style="text-align:center"> OUR MISSION </h3>
+
+    <p style="text-align:center">Develop a prototype cardiovascular data search engine.</p>
     <div class="about_box" style="background-color:white">
-       <div id='contact_form_errorloc' class='err'></div>
+    		<br>
+    
+    		<p class="about_style">The main purpose of this website is to demonstrate a prototype cardiovascular data search engine. Its goal is to discover data sets across data repositories or data aggregators and present them through the 
+    			interface that has been designed and developed based on the latest design and usability principals, making it a hub for researchers to fulfill their searching needs.
+    			In addition, the website supports the NIH-endorsed<a style="color:#e56969" href="http://www.nature.com/articles/sdata201618"> FAIR </a> principles of Findability, Accessibility, Interoperability
+            and Reusability of datasets with current functionality assisting in finding datasets and providing access information about them.</p>
+
+        <p class="about_style">The data repositories included in this initial release have been selected by the team and represent a sample of cardiovascular datasets that are currently avaliable. 
+        		We will include more datasets as we further develop the website.</p>
+
+        <p class="about_style">Currently, AHA indexes the core metadata available for most datasets, but it offers enhanced search functions when repositories provide additional metadata.
+            The Data Tag Suite <a style="color:#e56969" href ="https://biocaddie.org/group/working-group/working-group-3-descriptive-metadata-datasets"> (DATS)</a> metadata specification,
+            v2.2 is the underlying metadata model which has evolved with input from the biomedical informatics community.</p>
+
+        <p class="about_style">Please provide <a style="color:#e56969" href="./feedback.php"> suggestions </a>  on data repositories that we should cover as well as the quality of search results. This is a prototype in development and your valuable suggestions and comments will help us improve the system.</p>
+
+        <p class="about_style" style="color: #e56969; font-size: 12px;"> * We respect your privacy and will not share your personal information except as needed for access to DataMed. </p>
+
+        <p class="about_style" >   Contact us:</p>
+    
+       	<div id='contact_form_errorloc' class='err'></div>
         <form method="POST" name="contact_form" onsubmit="return confirm('Are you sure you want to submit?');"
               action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" class="about_style" data-toggle="validator">
             <div class="about_style" >
-                <div class="form-group">
+                
                 <label for="pwd">Name *</label>
                 <input type="text" aria-label="Enter your name" name="NAME" class="form-control" id="pwd" placeholder="Your name" required>
-                </div>
+
                 <label for="email">E-mail address *</label>
                 <input type="email" aria-label="Enter your email" name="EMAIL" class="form-control" id="email" placeholder="Your email"required>
                 <div class="form-group">

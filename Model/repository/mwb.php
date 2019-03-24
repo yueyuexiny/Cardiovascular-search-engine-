0ab7ea2df10385273ca7b2661b1c7d81bccb4112
yueyuexiny <xhu7@localhost>
2019-03-24 16:51:28 -0500

    fix mwb show array problem

--- Model/repository/mwb.php
+++ Model/repository/mwb.php
@@ -19,24 +19,54 @@ class mwbRepository extends RepositoryBase {
     public $type = 'dataset';
 
     //search page
-    public $searchPageField = ['dataset.title','dataset.identifier.identifier', 'dataset.description'];
+    
+    public $searchPageField = ['dataset.title', 'dataset.description'];
+
     public $searchPageHeader = [
         'dataset.title' => 'Title',
-        'dataset.identifier.identifier'=>'ID',
         'dataset.description' => 'Description'
     ];
 
 
     //search-repository page
-    public $searchRepoHeader = ['Dataset Title', 'ID','Description'];
-    public $searchRepoField = [ 'dataset.title', 'dataset.identifier.identifier','dataset.description'];
+    public $searchRepoHeader = ['Dataset Title', 'Description'];
+    public $searchRepoField = [ 'dataset.title', 'dataset.description'];
 
     public $source_main_page = 'https://www.metabolomicsworkbench.org/';
     //public $sort_field = 'dataset.dateReleased';
     public $description = 'Metabolomics Workbench serves as a public repository for metabolomics metadata and experimental data from different species including all major taxonomic categories, multiple experimental platforms, metabolite standards, metabolite structures, protocols, tutorials, training materials and other educational resources. It also provides computational and data analytics tools for large volumes of data from a wide variety of metabolomics studies including mass spectrometry (MS) and nuclear magnetic resonance spectrometry (NMR).';
 
+    public function getDisplayItemView($rows)
+    {
+        $search_results = parent::getDisplayItemView($rows);
+        
+        #print_r($rows['dataset']['2']);
+        #print_r($search_results['dataset']['2'][1]);
+               
+        $search_results['dataset']['2'][1] = $rows['dataset']['creators'][1]['firstName'] . " " . $rows['dataset']['creators'][1]['lastName'] . " of " . $rows['dataset']['creators'][1]['affiliations'][0]['name'];
 
+               
+        if($search_results['dataset']['3'][0]=='dates'){                   
+            if($search_results['dataset']['3'][1]=="Array"){
+                $search_results['dataset']['3'][1]=$rows['dataset']['dates'][0]['dates'][0]['type']['value'] . " : ". $rows['dataset']['dates'][0]['dates'][0]['date'] ;
+            }
+        }
+        
+        if($search_results['dataset']['4'][0]=='extraProperties'){
+            if($search_results['dataset']['4'][1]=="Array"){
+                $search_results['dataset']['4'][1]=$rows['dataset']['extraProperties'][0]['category'] . " : ". $rows['dataset']['extraProperties'][0]['values'][0]['value'];
+            }
+        }
+             
+        if($search_results['dataset']['5'][0]=='types'){
+            if($search_results['dataset']['5'][1]=="Array"){
+                $search_results['dataset']['5'][1]=$rows['dataset']['types'][0]['information']['value'];
+            }
+        }
 
+        return $search_results;       
+        
+    }
 
 }
 
